0293078e881019eb39bacbcd5db9067820aeb7c4
Erin Hong <erin.hong17@gmail.com>
2016-02-17 02:25:59 -0500

    finished all possibly

--- translate_game.js
+++ translate_game.js
@@ -1,38 +1,81 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
 $(function() {
-	//try to make this not inside function 
-	function generateRandomWord(){
-		var test_word = test_words[Math.floor(Math.random()*current_dict_length)]; 
-		return test_word;  
-	}
+	
+	var lang_to	= "English";
+	var lang_from = "Spanish";
+	var current_dict = dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
+	var current_dict_length = Object.keys(current_dict).length;
+	var answer_words = Object.keys(current_dict); 
+	var keys = Object.keys(current_dict); 
 
-	function getKeyByValue(dictionary,value){
-		for (var key_index in Object.keys(dictionary)){
-			var current_key = Object.keys(dictionary)[key_index]; 
-			var current_value = dictionary[current_key];
-			if (current_value == value){ 
-				return current_key; 
+	var current_answer = ""; 
+	var current_test_word = ""; 
+	var correctness = false; 
+	var guess = document.getElementById("user_guess"); 
+	guess.focus(); 
+
+	//First generated word 
+	var next = generateRandomWord();
+	updateTestWord(current_test_word); 
+
+	//Code for when user wants to see answer 
+	var button = $("#see_answer_button"); 
+	$(button).click(function(){
+
+		submit(guess.value); 
+
+		//Reset the text field and re-focus on the text box 
+		guess.value=""; 
+		guess.focus(); 
+	}); 
+
+	$(guess).keyup(function(e){
+		if (e.keyCode ==13){
+			// if entered while autocomplete was closed
+			if (!!$($("#user_guess").autocomplete('widget')).is(":visible")){
+				$("#user_guess").autocomplete("close"); 
+				submit(guess.value); 
+				guess.value = ""; 
+				guess.focus(); 
 			}
-		}return ""; 
-	}
-
-	function getValueByKey(dictionary,key){
-		alert("key I received: "+key);
-		alert("what I get from dictionary: "+dictionary[key]); 
-		alert(dictionary[key]!=undefined); 
-		if (dictionary[key]!=undefined){
-			return dictionary[key];
 		}
-		return ""; 
+	})
+
+
+	$("#user_guess").autocomplete({
+		source: keys,
+		minLength:2, 
+		select: function(event,ui){
+
+			var selected_guess = ui.item.value; 
+			submit(selected_guess); 
+
+			//clear and re-focus 
+			$(this).val("");
+			$(this).focus(); 
+			return false;
+		}
+	});
+
+	function submit(guess){
+		correctness = checkGuess(guess); 
+		addEntry(current_test_word,guess,current_answer,correctness); 
+		generateRandomWord(); 
+		updateTestWord(current_test_word); 
 	}
 
-	function checkGuess(dictionary,test_word,guess){
-		var correct_answer = getKeyByValue(dictionary,test_word); // switched GetKeyByValue 
-		if (correct_answer != ""){
-			if (correct_answer == guess){
+	function generateRandomWord(){
+		var answer = answer_words[Math.floor(Math.random()*current_dict_length)];
+		var test_word =  current_dict[answer]; 
+		current_answer = answer; 
+		current_test_word = test_word; 
+		return [test_word,answer];
+	}
+
+	function checkGuess(guess){ 
+		if (current_answer == guess){
 				return true; 
-			}
 		}
 	return false; 
 	}
@@ -42,21 +85,23 @@ $(function() {
 		$(text_box).html(test_word); 
 	}
 
-	function addEntry(test_word,guess,real_answer,correct){
+	function addEntry(test_word,guess,current_answer,correctness){
 		var table = document.getElementById("table");
 		var new_row = table.insertRow(2);
 		var existing_rows = 0; 
+
 		if ($(".history_row")[0]){
 			existing_rows = $(".history_row").length; 
 		}
+
 		var new_id = "row"+existing_rows+1; 
 		$(new_row).attr("id",new_id); //#row1, #row2! 
 		$(new_row).addClass("history_row"); 
 		var col1 = document.createElement("td"); //test_word
 		var col2 = document.createElement("td"); // guess
-		var col3 = document.createElement("td"); // real_answer 
+		var col3 = document.createElement("td"); // current_answer 
 
-		if (!correct){//if answer is wrong 
+		if (!correctness){//if answer is wrong 
 			
 			$(col1).addClass("red");
 			col1.textContent = test_word; 
@@ -66,7 +111,7 @@ $(function() {
 			col2.textContent = guess;
 			
 			$(col3).addClass("red"); 
-			col3.textContent = real_answer;
+			col3.textContent = current_answer;
 
 		}
 		else{//answered correctly! 
@@ -74,7 +119,7 @@ $(function() {
 			col1.textContent = test_word; 
 
 			$(col2).addClass("blue");
-			col2.textContent = real_answer; 
+			col2.textContent = current_answer; 
 
 			col3.textContent = "✓"; 
 		}
@@ -83,53 +128,5 @@ $(function() {
 		$(new_row).append(col3); 
 
 	}
-
-//add entry 
-
-	document.getElementById("user_guess").focus();
-	var lang_to	= "English";
-	var lang_from = "Spanish";
-	var current_dict = dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
-	var current_dict_length = Object.values(current_dict).length;
-	var test_words = Object.values(current_dict); 
-	var keys = Object.keys(current_dict); 
-
-
-
-	//First generated word 
-	var current_test_word = generateRandomWord(test_words);
-	updateTestWord(current_test_word); 
-
-	//Code for when user wants to see answer 
-	var button = $("#see_answer_button"); 
-	$(button).click(function(){
-
-		var guess = document.getElementById("user_guess"); 
-		var correct = checkGuess(current_dict,current_test_word,guess.value); 
-		var real_answer = getKeyByValue(current_dict,current_test_word);
-		alert(correct);
-		addEntry(current_test_word,guess.value,real_answer,correct); 
-
-		//Reset the text field and re-focus on the text box 
-		current_test_word = generateRandomWord();
-		updateTestWord(current_test_word); 
-		guess.value=""; 
-		guess.focus(); 
-	}); 
-
-	$("#user_guess").autocomplete({
-		source: keys,
-		minLength:2, 
-		select: function(event,ui){
-			var selected_guess = ui.item.value; 
-			
-			$(this).val("");
-			$(this).focus(); 
-			return false;
-		}
-	});
-
-
-
 });
 
